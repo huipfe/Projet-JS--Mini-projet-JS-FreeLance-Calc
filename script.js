4c2c4414f96a4dc152eb0e83ecf57678c7bad5d9
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-02-16 15:46:20 +0100

    Ajout de Stocker le formulaire en cookie

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //Faire un script qui permet de se calculer, ce qu'on gagne, en tant que freelance. Salaire brut, avec taxe, et salaire net.
 
 function CalculGain () {
+
+    //On vérifie les inputs
+    verifierInputs();
+
     /**
      * On récupère le formulaire dans le HTML =>
      * On récupère l'id dans la balise form.
@@ -9,7 +13,8 @@ function CalculGain () {
 
     //On le transforme en objet FormData
     let formObj = new FormData(myForm);
-    
+    //Récupérer les cookies (données stocker
+    saveElementInCookies(formObj)
 
     //On récupère les inputs de notre formulaire par leur noms 
 
@@ -41,7 +46,7 @@ function CalculGain () {
     
     //Mettre le résultat de totalBrut dans le boutton BRUT
     //Pour se faire, on récupère l'id du boutton rouge BRUT
-    document.getElementById("resultatBrut").innerText = totalBrut + " €"
+    document.getElementById("resultatBrut").innerText = totalBrut.toFixed(2) + " €"
     //Et on lui change son texte, pour afficher le résultat de totalBrut, avec innerText = le total Brut + le sigle euro en caractère
     
 
@@ -54,19 +59,87 @@ function CalculGain () {
     let totalNet = totalBrut - chargeADeduire;
     
     //Résultat charges à déduire
-    document.getElementById("resultatDifference").innerText = chargeADeduire + " €"
+    document.getElementById("resultatDifference").innerText = chargeADeduire.toFixed(2) + " €"
 
     //Résultat Net 
-    document.getElementById("resultatNet").innerText = totalNet + " €"
+    document.getElementById("resultatNet").innerText = totalNet.toFixed(2) + " €"
 
 
 }
 
+
+//Fonction pour sauve ses derniers calculs, dans des cookies.
+
+function saveElementInCookies(input) {
+    document.cookie = input.name + '=' + input.value;
+}
+
+//Récupérer les cookies, et mettre les valeurs, dans les inputs
+function getCookie(input){
+    let mesCookies = document.cookie;
+    // TH = 1; TJM = 2; extras = 3; QteTH= 4; QteTJM = 5; QteExtra = 6; charges = 7.
+
+    const name = input.name + '='; 
+    const tableauCookies = mesCookies.split('; ');
+    let valeurCookie = null;
+    tableauCookies.forEach(cookie => {
+        if (cookie.indexOf(name) === 0) {
+            //On a le bon cookie
+            valeurCookie = cookie.substring(name.length);
+            console.log(valeurCookie);
+        }
+    });
+    return valeurCookie;
+}
+
+//Ne plus déclencher l'event via un onclick en html, mais directement depuis un event JS, ajout d'évent
+let btn = document.getElementById("buttonValidation")
+btn.addEventListener("click", CalculGain);
+
+//Raffraichir le résultat au changement de valeur via l'input
+let mesInputs = document.querySelectorAll("#formCalculGain input.form-control");
+
+//Parcourir mes éléments, via un tableau, et rajouter onchange
+mesInputs.forEach(monInput => {
+
+    //Si il a une valeur en cookie, lui donner
+    let cookie = getCookie(monInput);
+
+        if (cookie != undefined && cookie != null ){
+            monInput.value = cookie;
+            
+        }
+
+
+
+    monInput.addEventListener('keyup', CalculGain);
+    monInput.addEventListener('change', CalculGain);
+
+});
+
+CalculGain();
+
+function verifierInputs() {
+    //Raffraichir le résultat au changement de valeur via l'input
+    let mesInputs = document.querySelectorAll("#formCalculGain input.form-control");
+
+    mesInputs.forEach(monInput => {
+            //Vérifier s'il vaut 0 ou plus
+            //Si il est inférieur à zéro
+        if (monInput.value < 0 ){
+                //Alors on lui met zéro
+                monInput.value = 0;
+
+            }
+        saveElementInCookies(monInput);
+    });
+}
+
+
+
 /** Fonctionnalités supplémentaires, à faire si possible. 
- * Arrondir le résultat, pour ne pas avoir de résultat trop long, dans taxes, deux décimals max. 
- * Raffraichir le résultat au changement d'input (unchange et keyup)
- * Vérifier les données (si <0)
- * Stocker en cookie le formulaire, pour le remplir avec notre dernier calcul
+
+
  * - Potentiellement, mais optionel - 
  * Historique de calcul ? (seulement si clic pour calculer)
  * Imprimer la page ? 
@@ -75,4 +148,12 @@ function CalculGain () {
  * Envoyer Mail ?
  */
 
+/** Fonctionnalité supplémentaire, faites.
+ * Arrondir le résultat, pour ne pas avoir de résultat trop long, dans taxes, deux décimals max. = ajout après la valeur du résultat final : .toFixed(2) 
+ * Raffraichir le résultat au changement d'input (unchange et keyup) : .forEach (tableau avec un foreach)
+ * Vérifier les données (si < 0), via la fonction : verifierInputs()
+ * Stocker en cookie le formulaire, pour le remplir avec notre dernier calcul : getCookie() & saveElementInCookies ()
+ * 
+ * 
+ */
 
